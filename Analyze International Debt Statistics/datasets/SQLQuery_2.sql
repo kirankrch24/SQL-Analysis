e02eb07e6754ce0b986d75ea07b2d36d797d3e87
Kiran Kumar Chaudhary <kirankr24@outlook.com>
2023-04-17 15:36:50 +0100

    e

--- Analyze International Debt Statistics/datasets/SQLQuery_2.sql
+++ Analyze International Debt Statistics/datasets/SQLQuery_2.sql
@@ -11,7 +11,3 @@ CREATE TABLE international_debt
   debt numeric
 );
 
-BULK INSERT international_debt
-FROM 'Desktop/Projects/SQL-Analysis/Analyze International Debt Statistics/datasets/international_debt.csv'
-WITH (FORMAT = 'CSV', FIELDTERMINATOR = ',', FIRSTROW = 2);
-
